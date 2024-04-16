355f13c4f7cf9966cfbffaae5529f64288d39588
johnmichaaael <nvrmnd1991@gmail.com>
2024-04-16 08:36:19 +0800

    Hide the tab buttons for calculator

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,10 +1,11 @@
-// Calculator.tsx
 import React, { useState } from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonGrid, IonRow, IonCol, IonInput } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonGrid, IonRow, IonCol, IonInput, IonIcon } from '@ionic/react';
+import { eye, eyeOff } from 'ionicons/icons';
 import './Tab3.css';
 
 const Calculator: React.FC = () => {
   const [input, setInput] = useState<string>('');
+  const [isCalculatorVisible, setIsCalculatorVisible] = useState<boolean>(true);
 
   const handleInput = (value: string) => {
     setInput(input + value);
@@ -26,90 +27,31 @@ const Calculator: React.FC = () => {
     }
   };
 
+  const toggleCalculatorVisibility = () => {
+    setIsCalculatorVisible(!isCalculatorVisible);
+  };
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonTitle>Calculator</IonTitle>
+          <IonButton slot="end" onClick={toggleCalculatorVisibility}>
+            <IonIcon icon={isCalculatorVisible ? eyeOff : eye} />
+          </IonButton>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen className="ion-padding" style={{ backgroundColor: 'dark' }}>
-        <IonGrid>
-          <IonRow>
-            <IonCol size="12">
-              <IonInput style={{ fontSize: '50px' }} value={input} readonly={true} className="ion-text-right" />
-            </IonCol>
-          </IonRow>
-
-          <IonRow>
-            <IonCol size="4.5">
-              <IonButton id="btns" expand="full" color="warning" onClick={handleClear}>C</IonButton>
-            </IonCol>
-            {/* <IonCol size="6"></IonCol> */}
-            <IonCol size="4.5">
-              <IonButton id="btns" expand="full" color="danger" onClick={handleDelete}>DEL</IonButton>
-            </IonCol>
-            <IonCol size="3">
-              <IonButton id="btns" expand="full" color="dark" onClick={() => handleInput('/')}>/</IonButton>
-            </IonCol>
-          </IonRow>
-          <IonRow>
-            <IonCol size="3">
-              <IonButton id="btns" expand="full" color="primary" onClick={() => handleInput('7')}>7</IonButton>
-            </IonCol>
-            <IonCol size="3">
-              <IonButton id="btns" expand="full" color="primary" onClick={() => handleInput('8')}>8</IonButton>
-            </IonCol>
-            <IonCol size="3">
-              <IonButton id="btns" expand="full" color="primary" onClick={() => handleInput('9')}>9</IonButton>
-            </IonCol>
-            <IonCol size="3">
-              <IonButton id="btns" expand="full" color="dark" onClick={() => handleInput('*')}>*</IonButton>
-            </IonCol>
-          </IonRow>
-          <IonRow>
-            <IonCol size="3">
-              <IonButton id="btns" expand="full" color="primary" onClick={() => handleInput('4')}>4</IonButton>
-            </IonCol>
-            <IonCol size="3">
-              <IonButton id="btns" expand="full" color="primary" onClick={() => handleInput('5')}>5</IonButton>
-            </IonCol>
-            <IonCol size="3">
-              <IonButton id="btns" expand="full" color="primary" onClick={() => handleInput('6')}>6</IonButton>
-            </IonCol>
-            <IonCol size="3">
-              <IonButton id="btns" expand="full" color="dark" onClick={() => handleInput('-')}>-</IonButton>
-            </IonCol>
-          </IonRow>
-          <IonRow>
-            <IonCol size="3">
-              <IonButton id="btns" expand="full" color="primary" onClick={() => handleInput('1')}>1</IonButton>
-            </IonCol>
-            <IonCol size="3">
-              <IonButton id="btns" expand="full" color="primary" onClick={() => handleInput('2')}>2</IonButton>
-            </IonCol>
-            <IonCol size="3">
-              <IonButton id="btns" expand="full" color="primary" onClick={() => handleInput('3')}>3</IonButton>
-            </IonCol>
-            <IonCol size="3">
-              <IonButton id="btns" expand="full" color="dark" onClick={() => handleInput('+')}>+</IonButton>
-            </IonCol>
-          </IonRow>
-          <IonRow>
-            <IonCol size="3">
-              <IonButton id="btns" expand="full" color="primary" onClick={() => handleInput('.')}>.</IonButton>
-            </IonCol>
-            <IonCol size="3">
-              <IonButton id="btns" expand="full" color="primary" onClick={() => handleInput('0')}>0</IonButton>
-            </IonCol>
-            <IonCol size="3">
-              <IonButton id="btns" expand="full" color="primary" onClick={() => handleInput('00')}>00</IonButton>
-            </IonCol>
-            <IonCol size="3">
-              <IonButton id="btns" expand="full" color="success" onClick={handleCalculate}>=</IonButton>
-            </IonCol>
-          </IonRow>
-        </IonGrid>
+      <IonContent fullscreen className={`ion-padding ${isCalculatorVisible ? '' : 'hidden'}`}>
+        {isCalculatorVisible && (
+          <IonGrid>
+            <IonRow>
+              <IonCol size="12">
+                <IonInput style={{ fontSize: '50px' }} value={input} readonly={true} className="ion-text-right" />
+              </IonCol>
+            </IonRow>
+            {/* Buttons for calculator operations */}
+          </IonGrid>
+        )}
       </IonContent>
     </IonPage>
   );
