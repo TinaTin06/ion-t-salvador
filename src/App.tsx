c9c32a424b087fdb2695ae0e56e912b671bdc018
Your Name <salvador.dessy@gmail.com>
2024-04-19 03:37:11 +0200

    Added cards on the homepage

--- src/App.tsx
+++ src/App.tsx
@@ -11,26 +11,25 @@ import {
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { calculatorOutline, fingerPrintOutline, personCircleOutline, homeOutline, eyeOutline, eyeOffOutline } from 'ionicons/icons'; // Added eyeOutline and eyeOffOutline
+import {
+  calculatorOutline,
+  fingerPrintOutline,
+  personCircleOutline,
+  homeOutline,
+  eyeOutline,
+  eyeOffOutline
+} from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
 import Home from './pages/home';
-
-/* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
 import '@ionic/react/css/normalize.css';
 import '@ionic/react/css/structure.css';
 import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
 import '@ionic/react/css/padding.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
-
-/* Theme variables */
 import './theme/variables.css';
 
 setupIonicReact();
@@ -48,11 +47,7 @@ const App: React.FC = () => {
         <IonTabs>
           <IonRouterOutlet>
             <Route path="/tab1" component={Tab1} exact />
-            {!isTabHidden && (
-              <>
-                <Route path="/tab2" component={Tab2} exact />
-              </>
-            )}
+            {!isTabHidden && <Route path="/tab2" component={Tab2} exact />}
             <Route path="/tab3" component={Tab3} exact />
             <Route path="/home" component={Home} exact />
             <Route exact path="/">
@@ -61,27 +56,25 @@ const App: React.FC = () => {
           </IonRouterOutlet>
           <IonTabBar slot="bottom">
             <IonTabButton tab="tab1" href="/tab1">
-              <IonIcon aria-hidden="true" icon={personCircleOutline} />
+              <IonIcon icon={personCircleOutline} />
               <IonLabel>Profile</IonLabel>
             </IonTabButton>
             {!isTabHidden && (
-              <>
-                <IonTabButton tab="tab2" href="/tab2">
-                  <IonIcon aria-hidden="true" icon={fingerPrintOutline} />
-                  <IonLabel>Click Counter</IonLabel>
-                </IonTabButton>
-              </>
+              <IonTabButton tab="tab2" href="/tab2">
+                <IonIcon icon={fingerPrintOutline} />
+                <IonLabel>Click Counter</IonLabel>
+              </IonTabButton>
             )}
             <IonTabButton tab="tab3" href="/tab3">
-              <IonIcon aria-hidden="true" icon={calculatorOutline} />
+              <IonIcon icon={calculatorOutline} />
               <IonLabel>Calculator</IonLabel>
             </IonTabButton>
             <IonTabButton tab="home" href="/home">
-              <IonIcon aria-hidden="true" icon={homeOutline} />
+              <IonIcon icon={homeOutline} />
               <IonLabel>Home</IonLabel>
             </IonTabButton>
             <IonTabButton onClick={toggleTabVisibility}>
-              <IonIcon aria-hidden="true" icon={isTabHidden ? eyeOutline : eyeOffOutline} />
+              <IonIcon icon={isTabHidden ? eyeOutline : eyeOffOutline} />
               <IonLabel>{isTabHidden ? 'Show Tab' : 'Hide Tab'}</IonLabel>
             </IonTabButton>
           </IonTabBar>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,22 +1,70 @@
 import React from 'react';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonIcon,
+  IonCard,
+  IonCardContent,
+} from '@ionic/react';
+import { pulseOutline, calculatorOutline } from 'ionicons/icons';
+import { useHistory } from 'react-router-dom';
+import './home.css'; // Styling for the Home component
 
 const Home: React.FC = () => {
-    return (
-        <div>
-            <h1>Welcome to my Ion-T-Salvador</h1>
-            <Bio />
-            {/* Other content of the home page goes here */}
-        </div>
-    );
-};
+  const history = useHistory();
 
-const Bio: React.FC = () => {
-    return (
-        <div>
-            <h2>Bio</h2>
-            <p>This is my bio.</p>
+  // Navigate to the Click Counter page
+  const goToClickCounter = () => {
+    history.push('/click_counter');
+  };
+
+  // Navigate to the Calculator page
+  const goToCalculator = () => {
+    history.push('/calculator');
+  };
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Home</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen className="ion-padding">
+        <div className="ion-text-center">
+          <IonCard className="home-card" onClick={goToClickCounter}>
+            <IonCardContent className="home-card-content">
+              <IonIcon icon={pulseOutline} slot="start" aria-label="Click Counter" />
+              Click Counter
+            </IonCardContent>
+          </IonCard>
+  
+          <IonCard className="home-card" onClick={goToCalculator}>
+            <IonCardContent className="home-card-content">
+              <IonIcon icon={calculatorOutline} slot="start" aria-label="Calculator" />
+              Calculator
+            </IonCardContent>
+          </IonCard>
+          
+          {/* Placeholder cards for future implementation */}
+          <IonCard className="home-card">
+            <IonCardContent className="home-card-content">
+              Future Feature
+            </IonCardContent>
+          </IonCard>
+  
+          <IonCard className="home-card">
+            <IonCardContent className="home-card-content">
+              Future Feature
+            </IonCardContent>
+          </IonCard>
         </div>
-    );
+      </IonContent>
+    </IonPage>
+  );
 };
 
 export default Home;
